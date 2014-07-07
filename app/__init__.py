02e8e2c057bf53716ac83068b1b4d6f7b5f91824
lawphill <lawphill@gmail.com>
2014-07-07 14:59:27 -0700

    Added extra logging statements in init file

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,11 @@
 from flask import Flask
 
+print "Init file was called"
 app = Flask(__name__)
 app.config.from_object('config')
 
-print "App was called"
+print "App configured"
 
 from app import views
+
+print "Views was imported"
